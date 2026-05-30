3c9a5406ed393cf7a3465e976bfd4d100043bf6a
Network Automation Solutions <networkautomation311@gmail.com>
2026-05-30 16:36:59 +0530

    Update nodemailer.js

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,12 +1,17 @@
-
 const nodemailer = require('nodemailer')
 require('dotenv').config()
 
 const transporter = nodemailer.createTransport({
-    service:"gmail",
+    host: "smtp.gmail.com",
+    port: 465,         // Hostinger के लिए पोर्ट 465 सबसे बेस्ट और सिक्योर है
+    secure: true,      // पोर्ट 465 के लिए इसे true रखना ज़रूरी है
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS    
+    },
+    tls: {
+        // यह लाइन Hostinger सर्वर और Google के बीच अनधिकृत एरर (SSL Certificate issue) को रोकती है
+        rejectUnauthorized: false 
     }
 })
 
